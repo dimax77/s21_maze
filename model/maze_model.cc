9cecfc5a519c1fda4d42ba42bee704d0439c344c
mockingb <m.1@bk.ru>
2023-11-29 23:14:42 +0300

    smart pointers injected

--- model/maze_model.cc
+++ model/maze_model.cc
@@ -10,19 +10,7 @@ MazeModel::MazeModel(std::shared_ptr<MazeState> state) : state_(state) {
 }
 
 void MazeModel::GenerateMaze(int height, int width) {
-  auto v = QVector(height, QVector<int>(width, 1));
-  for (int r = 0; r < height; ++r) {
-    for (int c = 0; c < width; ++c) {
-      std::cout << v[r][c];
-    }
-    std::cout << std::endl;
-  }
-  if (state_) {
-    std::cout << "Trying draw maze..\n";
-    state_->value_ = v;
-
-    state_->Notify(1);
-  } else {
-    std::cout << "Maze State is uninitialized.\n ";
-  }
+  auto test_maze = QVector(height, QVector<int>(width, 1));
+  state_->value_ = test_maze;
+  state_->Notify(1);
 }
